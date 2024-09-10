a52aaf9adea445dff9edecb0573d060202ef900d
乙米 <loujiachen.ljc@alibaba-inc.com>
2024-09-10 10:08:12 +0800

    feat: initial commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"chishane-2gsirlz919069514","alias":"chishane"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e chishane-2gsirlz919069514 --n quickstartFunctions --r --project "/Users/alsc/Desktop/study/chishadiansha" --report_first --report
