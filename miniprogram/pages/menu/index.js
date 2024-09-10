d4144318a684271e2475c081f2165fae6b03d1a7
techcoola <102975936@qq.com>
2024-09-10 23:31:17 +0800

    feat: 增加已点菜单

--- miniprogram/pages/menu/index.js
+++ miniprogram/pages/menu/index.js
@@ -1,67 +1,129 @@
-const { envList } = require("../../envList");
-const { QuickStartPoints, QuickStartSteps } = require("./constants");
-import { fetchMenu } from './mixin'
+const {
+  envList
+} = require("../../envList");
+const {
+  QuickStartPoints,
+  QuickStartSteps
+} = require("./constants");
+import {
+  fetchMenu
+} from './mixin'
 
 
 Page({
   data: {
-    menuData:null,
-    typeList:[],
-    activeIndex:0
+    menuData: null,
+    typeList: [],
+    activeIndex: 0,
+    position:null,
+    show: false,
+    customStyle:null,
+    duration:1000,
+    orderList:[]
   },
 
   async onLoad() {
     const data = await fetchMenu();
     const menuData = data.result.dataList;
     const menuMap = {};
-    menuData.forEach( item => {
+    menuData.forEach(item => {
       const type = item.type;
-      if(!menuMap[type]) {
+      if (!menuMap[type]) {
         menuMap[type] = [];
       }
       menuMap[type].push(item);
     });
     const typeList = Object.keys(menuMap);
-    console.log('menuMap',menuMap);
-    this.setData({menuData:menuMap,typeList})
+    console.log('menuMap', menuMap);
+    this.setData({
+      menuData: menuMap,
+      typeList
+    })
   },
 
   clickType(e) {
     const index = e.currentTarget.dataset.index;
-    console.log('activeIndex',index);
-    this.setData({activeIndex:index})
+    console.log('activeIndex', index);
+    this.setData({
+      activeIndex: index
+    })
   },
 
   add(e) {
-    const {id,type} = e.currentTarget.dataset;
+    const {
+      id,
+      type
+    } = e.currentTarget.dataset;
     const menuData = this.data.menuData;
     const dataList = menuData[type];
     const item = dataList.find(item => item.name == id);
-    if(!item.count) {
+    if (!item.count) {
       item.count = 1;
     } else {
-      item.count ++;
+      item.count++;
+    };
+    const addName = item.name;
+    const orderList =this.data.orderList;
+    console.log('addName',addName);
+    console.log('index',orderList.indexOf(item => item.name === addName));
+
+    if(!orderList.some(item => item.name === addName) ) {
+      orderList.push(item);
     }
-    this.setData({menuData})
-    console.log('item',item);
-    console.log('id',id);
+    console.log('orderList',orderList);
+    
+    this.setData({
+      menuData,
+      orderList
+    })
+    console.log('item', item);
+    console.log('id', id);
 
   },
 
   minus(e) {
-    const {id,type} = e.currentTarget.dataset;
+    const {
+      id,
+      type
+    } = e.currentTarget.dataset;
     const menuData = this.data.menuData;
     const dataList = menuData[type];
     const item = dataList.find(item => item.name == id);
-    if(!item.count) {
+    if (!item.count) {
       item.count = 0;
     } else {
-      item.count --;
+      item.count--;
     }
-    this.setData({menuData})
-    console.log('item',item);
-    console.log('id',id);
+    let orderList = this.data.orderList;
+    if(!item.count) {
+      const deleteName = item.name;
+       orderList = orderList.filter(item => item.name !== deleteName)
+    }
+    console.log('orderList',orderList);
+    this.setData({
+      menuData,
+      orderList
+    })
 
-  }
+  },
+  popup(e) {
+    const position = e.currentTarget.dataset.position
+    let customStyle = ''
+    let duration = this.data.duration
+    switch(position) {
+      case 'top':
+      case 'bottom': 
+    customStyle = 'height: 50%;'
+        break
+      case 'right':
+        break
+    }
+    this.setData({
+      position,
+      show: true,
+      customStyle,
+      duration
+    })
+  },
 
 });
